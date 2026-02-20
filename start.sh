66a9beff37f6b50a2b7147aa646aefdfa12bce5b
Sergey <sergey.khl.dev@gmail.com>
2026-02-20 01:20:14 -0700

    synch dino kg

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-rocker --nvidia --x11 --network host --name isaac_ros_bridge --privileged isaac_sim_ros:ubuntu_24_jazzy
+rocker --nvidia --x11 --network host --name isaac_ros_bridge --volume $(pwd)/IsaacSim-ros_workspaces/jazzy_ws/src/dino_kg:/workspace/jazzy_ws/src/dino_kg --privileged isaac_sim_ros:ubuntu_24_jazzy
