93e8341f5c425b3c2a5c4616fd4162d58f38834a
Sergey <sergey.khl.dev@gmail.com>
2026-02-25 21:17:47 -0700

    not breaking immediately when importing correspondences

--- build_dino_kg.sh
+++ build_dino_kg.sh
@@ -1,2 +1,2 @@
 # this should be run in the container. im just keeping it here as a note until I figure out better way
-colcon build --packages-select dino_kg --symlink-install --merge-install
+colcon build --packages-select dino_kg --symlink-install
