af1ca00b196fbb7dc5f5e70024f33de8c7353b42
Sergey <sergey.khl.dev@gmail.com>
2026-03-19 19:33:44 -0600

    i think this sim works

--- query.py
+++ query.py
@@ -22,13 +22,15 @@ def get_vec_for_query(df, query):
         
         if prev_match is not None:
             print(f"matched {prev_match}")
-            vecs.append(df.loc[prev_match].values)
+            vec = df.loc[prev_match].values
+            normalized_vec = vec / np.linalg.norm(vec)
+            vecs.append(normalized_vec)
             prev_match = None
             prev_idx = curr_idx
 
     return vecs
 
-def get_vecs_for_task(df, skills):
+def get_vecs_and_count_for_task(df, skills):
     filtered_skills = [skill for skill in skills if skill != "<HOME>"]
     skills_to_find_uris_for = [
         word 
@@ -36,10 +38,26 @@ def get_vecs_for_task(df, skills):
         for word in skill.split("_")
     ]
     uris = [standardized_uri('en', skill) for skill in skills_to_find_uris_for]
-    return [df.loc[uri].values for uri in uris if uri in df.index]
+    vecs = [df.loc[uri].values for uri in uris if uri in df.index]
+    
+    normalized_vecs = [vec / np.linalg.norm(vec) for vec in vecs]
 
+    return normalized_vecs, len(vecs)
+
+
+def find_task_similarity(query_vecs, task_vecs, normalizer):
+    sim = 0
+    for qvec in query_vecs:
+        for tvec in task_vecs:
+            sim += np.dot(qvec, tvec)
+
+    return sim / normalizer
+
+# NOTE: we are always working with normalized embeddings so dot product is always cos sim
 def main():
-    query = "prepare me a fruit salad"
+    # query = "prepare me a fruit salad"
+    # query = "lighting never strikes twice"
+    # query = "make me fruit, robot!"
 
 
     # these are the behaviour tree which represent full tasks
@@ -52,9 +70,19 @@ def main():
     df = df[whitelist_english]
 
     query_vecs = get_vec_for_query(df, query)
-    tasks_vecs = {}
+    task_sim = {}
     for task, skills in tasks.items():
-        tasks_vecs[task] = get_vecs_for_task(df, skills)
+        vecs, count = get_vecs_and_count_for_task(df, skills) 
+
+        sim = find_task_similarity(query_vecs, vecs, count*len(query.split()))
+
+        task_sim[task] = sim
+
+    print(task_sim)
+
+    winner = max(task_sim, key=task_sim.get)
+
+    print(f"The winning task is {winner} with a value of {task_sim[winner]}")
 
 
 if __name__ == "__main__":
