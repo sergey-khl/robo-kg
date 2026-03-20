95b8a0dc108c4cd3978dc2799a5d44b65f111014
Sergey <sergey.khl.dev@gmail.com>
2026-03-19 19:05:09 -0600

    finding vecs for tasks

--- query.py
+++ query.py
@@ -3,7 +3,7 @@ from text_to_uri import standardized_uri
 import numpy as np
 
 
-def get_vec_for_window(df, query):
+def get_vec_for_query(df, query):
     vecs = []
 
     query = query.split()
@@ -42,8 +42,20 @@ def main():
     whitelist_english = df.index.str.startswith('/c/en') # for our use case we only care about english
     df = df[whitelist_english]
 
-    query_vecs = get_vec_for_window(df, query)
-    print(query_vecs)
+    query_vecs = get_vec_for_query(df, query)
+    tasks_vecs = {}
+    for task, skills in tasks.items():
+        filtered_skills = [skill for skill in skills if skill != "<HOME>"]
+        skills_to_find_uris_for = [
+            word 
+            for skill in filtered_skills 
+            for word in skill.split("_")
+        ]
+        uris = [standardized_uri('en', skill) for skill in skills_to_find_uris_for]
+
+        tasks_vecs[task] = [df.loc[uri].values for uri in uris if uri in df.index]
+
+
 
 if __name__ == "__main__":
     main()
