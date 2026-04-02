396236b314ca9793e95160e0f177ceb4644516c5
Sergey <sergey.khl.dev@gmail.com>
2026-04-02 11:02:40 -0600

    add a little more

--- query.py
+++ query.py
@@ -1,22 +1,23 @@
 import pandas as pd
 from text_to_uri import standardized_uri
 import numpy as np
+import os
 
-
-def get_vecs_and_count_for_query(df, query):
+# this does fancy window slider which we use for our query and task name
+def window_parse(df, words):
     vecs = []
 
-    query = query.split()
+    words = words.split()
 
     prev_idx = 0
     prev_match = None
     curr_idx = 0
-    while curr_idx < len(query):
-        uri = standardized_uri('en', "-".join(query[prev_idx:curr_idx+1]))
+    while curr_idx < len(words):
+        uri = standardized_uri('en', "-".join(words[prev_idx:curr_idx+1]))
         if uri in df.index:
             prev_match = uri
             curr_idx += 1
-            if curr_idx != len(query):
+            if curr_idx != len(words):
                 continue
 
         
@@ -30,7 +31,8 @@ def get_vecs_and_count_for_query(df, query):
 
     return vecs, len(vecs)
 
-def get_vecs_and_count_for_task(df, skills):
+# this assumes each word in all our skills are part of the conceptnet graph
+def regular_parse(df, skills):
     filtered_skills = [skill for skill in skills if skill != "<HOME>"]
     skills_to_find_uris_for = [
         word 
@@ -38,6 +40,9 @@ def get_vecs_and_count_for_task(df, skills):
         for word in skill.split("_")
     ]
     uris = [standardized_uri('en', skill) for skill in skills_to_find_uris_for]
+
+    print(f"matched skills: {uris}")
+
     vecs = [df.loc[uri].values for uri in uris if uri in df.index]
     
     normalized_vecs = [vec / np.linalg.norm(vec) for vec in vecs]
@@ -62,27 +67,42 @@ def main():
 
     # these are the behaviour trees which represent full tasks
     tasks = {
-            "making_a_fruit_salad": ["robot_pickup_kiwi", "<HOME>", "item_drop_box", "<HOME>", "robot_pickup_strawberry", "<HOME>", "item_drop_box"],
-            "do_dishes": ["robot_pickup_cup", "<HOME>", "cup_place_dishwasher"],
-            "detonate_building": ["push_button"],
-            "start_car": ["push_button"],
-            "dust_the_furniture": ["robot_pickup_duster", "<HOME>", "duster_clean_photo", "<HOME>", "duster_clean_countertop", "<HOME>", "robot_putaway_duster"],
-            "🍞_🎥": ["🤖_pickup_🎥", "<HOME>", "🎥_capture_🍞", "<HOME>", "🤖_putaway_🎥"],
+            "making a fruit salad": ["robot_pickup_kiwi", "<HOME>", "item_drop_box", "<HOME>", "robot_pickup_strawberry", "<HOME>", "item_drop_box"],
+            "do dishes": ["robot_pickup_cup", "<HOME>", "cup_place_dishwasher"],
+            "detonate building": ["push_button"],
+            "start car": ["push_button"],
+            "dust the furniture": ["robot_pickup_duster", "<HOME>", "duster_clean_photo", "<HOME>", "duster_clean_countertop", "<HOME>", "robot_putaway_duster"],
+            "film bread": ["robot_pickup_camera", "<HOME>", "camera_capture_bread", "<HOME>", "robot_putaway_camera"],
             }
 
     df = pd.read_hdf('mini.h5')
     whitelist_english = df.index.str.startswith('/c/en') # for our use case we only care about english
-    whitelist_emojis = df.index.str.startswith('/c/mul') # and emojis
     df = df[whitelist_english]
 
-    query_vecs, query_count = get_vecs_and_count_for_query(df, query)
+    query_vecs, query_count = window_parse(df, query)
+
+    task_data = {}
+    for title, skills in tasks.items():
+        skill_vecs, skill_count = regular_parse(df, skills) 
+        title_vecs, title_count = window_parse(df, title) 
+
+        task_vecs = [*skill_vecs, *title_vecs]
+        task_count = skill_count + title_count
+        print("counts ", title_count, skill_count)
+        
+        task_data[title] = {
+            'vecs': task_vecs,
+            'count': task_count
+        }
+            
     task_sim = {}
-    for task, skills in tasks.items():
-        task_vecs, task_count = get_vecs_and_count_for_task(df, skills) 
+    for title, data in task_data.items():
+        task_vecs = data['vecs']
+        task_count = data['count']
 
         sim = find_task_similarity(query_vecs, task_vecs, query_count*task_count)
 
-        task_sim[task] = sim
+        task_sim[title] = sim
 
     print(task_sim)
 
